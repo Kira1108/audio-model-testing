2f033c76875ccdbdf335164247c1a3f480e2974b
Kira1108 <327485253@163.com>
2024-12-15 22:41:43 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Note on audio processing
+
+1. Install librosa and soundfile to handle audio files.
+2. Install pyaudio to perform real-time audio processing.[pyAudio documentation](!https://people.csail.mit.edu/hubert/pyaudio/), follow the instruction to install pyaudio.
+
+On macOS, we use the following method to install
+```bash
+brew install portaudio
+pip install pyaudio
+```
+3. Download asr model: there are many version of openai Whisper model on huggingface, we choose the tiny one for macOS testing purpose.
+
+4. We may need to install additional libraries such as `ffmpeg`, `libsndfile`, and `portaudio` to handle audio files and real-time audio processing. On macOS, we can use homebrew to install these libraries.
+```bash
+brew install ffmpeg
+brew install libsndfile
+brew install portaudio
+```
+
+> We may need to create a Docker container to run audio related code, such that the environment is independent of the host machine.
+
+
+pyaudio full duplex wire mode is useful in real-time audio processing.
+```python
+"""PyAudio Example: full-duplex wire between input and output."""
+
+import sys
+
+import pyaudio
+
+RECORD_SECONDS = 5
+CHUNK = 1024
+RATE = 44100
+
+p = pyaudio.PyAudio()
+stream = p.open(format=p.get_format_from_width(2),
+                channels=1 if sys.platform == 'darwin' else 2,
+                rate=RATE,
+                input=True,
+                output=True,
+                frames_per_buffer=CHUNK)
+
+print('* recording')
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    stream.write(stream.read(CHUNK))
+print('* done')
+
+stream.close()
+p.terminate()
+```
---
+++ whisper.py
@@ -0,0 +1,97 @@
+import logging
+logging.basicConfig(level = logging.INFO)
+from transformers import (
+    WhisperProcessor, 
+    WhisperForConditionalGeneration
+)
+import torch
+import logging
+from dataclasses import dataclass
+from functools import lru_cache
+import time
+
+def time_tracker(name: str):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            total_time = end_time - start_time
+            logging.info(f"{name} - Total time cost: {total_time:.4f} seconds")
+            return result
+        return wrapper
+    return decorator
+
+@lru_cache(maxsize = None)
+def load_asr_model(device:str = None):
+    logging.info("Loading Whisper model and processor")
+    processor = WhisperProcessor.from_pretrained("openai/whisper-tiny")
+    model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny")
+    model.config.forced_decoder_ids = None
+
+    if device is None:
+        device = "cuda" if torch.cuda.is_available() \
+            else "mps" if torch.backends.mps.is_available() \
+            else "cpu"
+        
+    device = torch.device(device)
+    model.to(device)
+    logging.info(f"Whisper model is running on {str(device)}")
+    return  model, processor, device
+
+@dataclass
+class WhisperASR:
+    
+    device:str = None
+    
+    def __post_init__(self):
+        self.model, self.processor, self.device = load_asr_model(self.device)
+        
+    def transcript_file(self, fp:str):
+        try:
+            import librosa
+        except:
+            raise ImportError("librosa is required for file processing")
+        
+        audio, sampling_rate = librosa.load(fp, sr=None)
+        
+        if sampling_rate != 16000:
+            audio = librosa.resample(audio, orig_sr=sampling_rate, target_sr=16000)
+            sampling_rate = 16000
+            
+        return self.transcript(audio, sampling_rate)
+        
+    def create_features(self, signal, sampling_rate):
+        "Converting audio signal to huggingface input features"
+        return self.processor(
+            signal, 
+            sampling_rate=sampling_rate, 
+            return_tensors="pt").input_features.to(self.device)
+    
+    def generate(self, input_features):
+        "Generate text tokens from input features"
+        return self.model.generate(input_features)
+    
+    def decode(self, predicted_ids):
+        "Decode token ids to text"
+        return self.processor.batch_decode(predicted_ids, skip_special_tokens=True)
+    
+    @time_tracker("ASR")
+    def transcript(self, signal, sampling_rate):
+        "Generate transcription from audio signal"
+        input_features = self.create_features(signal, sampling_rate)
+        predicted_ids = self.generate(input_features)
+        transcription = self.decode(predicted_ids)
+        return transcription
+    
+    def __call__(self, signal, sampling_rate):
+        "Generate transcription from audio signal"
+        return self.transcript(signal, sampling_rate)
+
+if __name__ == "__main__":
+    asr = WhisperASR(device="mps")
+    for _ in range(3):
+        transcription = asr.transcript_file("/Users/wanghuan/desktop/audio.m4a")
+        print(transcription)
+    
+
