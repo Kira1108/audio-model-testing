fb2d114b3eb9d42c1135896567494ddfa1735ce2
Kira1108 <327485253@163.com>
2024-12-17 10:22:10 +0800

    Initial commit

--- record_audio.py
+++ record_audio.py
@@ -1,27 +1,51 @@
-# 简单音频录制示例
-import wave
 import sys
-
 import pyaudio
+import struct
+import numpy as np
+from dataclasses import dataclass
+from typing import Any
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1 if sys.platform == 'darwin' else 2
-RATE = 44100
-RECORD_SECONDS = 5
+@dataclass
+class AudioRecorder:
+    chunk_size:int = 1024
+    audio_format:Any = pyaudio.paInt16
+    channels:int = None
+    rate:int = 16000
+    
+    def __post_init__(self):   
+        if self.channels is None:
+            self.channels = 1 if sys.platform == 'darwin' else 2
+        self.p = pyaudio.PyAudio()
+        
+    def gen_chunks(self, seconds:int = 10):
+        stream = self.p.open(
+            format=self.audio_format, 
+            channels=self.channels, 
+            rate=self.rate, 
+            input=True)
+        
+        print('Recording...')
+        for _ in range(0, self.rate // self.chunk_size * seconds):
+            chunk = stream.read(self.chunk_size)
+            fmt = f"{len(chunk) // 2}h"
+            samples = struct.unpack(fmt, chunk)
+            samples = np.array(samples, dtype = np.int16)
+            if self.channels == 2:
+                samples = samples.reshape(-1, 2)
+                samples = samples.mean(axis=1).astype(np.int16)
+            samples = samples / 32768.0
+            yield samples.tolist()
+        print('Recording Done...')
+        stream.close()
+        self.p.terminate()
+        
+if __name__ == "__main__":
+    agen = AudioRecorder()
+    samples = []
+    for chunk in agen.gen_chunks(5):
+        print(chunk[:20])
+        samples.extend(chunk)
+    print("Total samples:", len(samples))
+    
+    
 
-with wave.open('output.wav', 'wb') as wf:
-    p = pyaudio.PyAudio()
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-
-    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
-
-    print('Recording...')
-    for _ in range(0, RATE // CHUNK * RECORD_SECONDS):
-        wf.writeframes(stream.read(CHUNK))
-    print('Done')
-
-    stream.close()
-    p.terminate()
