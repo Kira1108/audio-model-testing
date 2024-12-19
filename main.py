afa63c08ce45984d99bb9218fe0fc2a1e35bfed1
Kira1108 <327485253@163.com>
2024-12-19 15:56:15 +0800

    Initial commit

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ from paraformer import Paraformer
 from vad import Vad
 
 def streaming_audio(fp = "datafiles/asr_example.wav"):
+    # 添加标点符号
     punc = PuncCreator()
+    # 静音检测
     vad = Vad()
     chunk_stride = 9600
+    # 语音识别
     paraformer = Paraformer()
     speech, sample_rate = soundfile.read(fp)
     total_chunk_num = int(len((speech)-1)/chunk_stride+1)
     buffer = ""
 
+    # 流失处理
     for i in range(total_chunk_num):
         speech_chunk = speech[i*chunk_stride:(i+1)*chunk_stride]
         is_final = i == total_chunk_num - 1
@@ -22,6 +26,7 @@ def streaming_audio(fp = "datafiles/asr_example.wav"):
             is_final
         )
         buffer += res
+        
         if vad.shutup(speech_chunk, is_final) and len(buffer) > 0:
             display = punc.create_punc(buffer)
             # 判断标点是否结束标点
