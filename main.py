7472af300501b79b7d1d83e648edde78b2581dc9
Kira1108 <327485253@163.com>
2024-12-19 15:33:40 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from recordings import AudioRecorder
-from whisper import WhisperASR
+import soundfile
+import os
+from punctuations import PuncCreator
+from paraformer import Paraformer
+from vad import Vad
 
-sampling_rate = 16000
-agen = AudioRecorder()
-whisper = WhisperASR()
+def streaming_audio(fp = "datafiles/asr_example.wav"):
+    punc = PuncCreator()
+    vad = Vad()
+    chunk_stride = 9600
+    paraformer = Paraformer()
+    speech, sample_rate = soundfile.read(fp)
+    total_chunk_num = int(len((speech)-1)/chunk_stride+1)
+    buffer = ""
 
-samples = []
-for chunk in agen.gen_chunks(seconds = 5):
-    samples.extend(chunk)
-    # print(whisper(chunk, sampling_rate))
-    
-print(whisper(samples, sampling_rate))
+    for i in range(total_chunk_num):
+        speech_chunk = speech[i*chunk_stride:(i+1)*chunk_stride]
+        is_final = i == total_chunk_num - 1
+        
+        res = paraformer.stream_asr(
+            speech_chunk, 
+            is_final
+        )
+        buffer += res
+        if vad.shutup(speech_chunk, is_final) and len(buffer) > 0:
+            display = punc.create_punc(buffer)
+        else:
+            display = buffer
+            
+        print(f"Current buffer [{i}]th: ", display)
+        
+if __name__ == "__main__":
+    streaming_audio()
